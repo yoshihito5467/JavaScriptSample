7b2983569457ffb1ff8a40ac0b2dd6a9e6aebe48
yoshihito <yoshihito>
2020-01-18 13:41:46 +0900

    モジュール JavaScriptSample の初期チェックイン

---
+++ JavaScriptSample/Models/ModalDialogModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace JavaScriptSample.Models
+{
+    public class ModalDialogModel
+    {
+        public string Country { get; set; }
+
+        public string UserId { get; set; }
+
+        public string UserName => string.IsNullOrWhiteSpace(this.UserId) ? "" : $"{this.UserId}さん";
+
+        public string CountryName => string.IsNullOrWhiteSpace(this.Country) ? "" : $"{this.Country}の国";
+
+        public ModalDialogModel()
+        {
+        }
+
+    }
+}
---
+++ JavaScriptSample/wwwroot/js/HomeIndex.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+
+    $(document).on("click", "#CallModalDialog", function () {
+        $.ajax({
+            url: _appSettings.AppRootPath + "Home/ModalDialog",
+            type: "POST",
+            data: {
+                "country": "ja-JP",
+                "userid": "999999",
+            }
+        }).done((data) => {
+            $("body").append($(data));
+            $("#ModalDialog").modal("show");
+        }).fail((data) => {
+            alert("fail" + data);
+        }).always((data) => {
+            // 正常終了でも異常終了でも常に実行される
+        });
+    });
+}); 
